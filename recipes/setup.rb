48d4007377c1101036871e0580338c11843d15f1
swathi <ec2-user@ip-172-31-95-253.ec2.internal>
2018-06-29 23:44:13 +0000

    default.rb includes detup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -14,9 +14,17 @@ package 'git' do
   action :install
 end
 
+#node['ipaddress']
+#node['memory']['total']
+
 file '/etc/motd' do
-  content 'This server is property of swathi'
-  action :create
+  content "This server is property of swathi
+  Hostname: #{node['hostname']}
+  Ipaddress: #{node['ipaddress']}
+  Cpu: #{node['cpu']['0']['mhz']}
+  Memory: #{node['memory']['total']}
+ "
   owner 'root'
   group 'root'
+  action :create
 end
